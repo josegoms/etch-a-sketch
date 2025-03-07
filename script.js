ae9903c2f43d81500494b88413d03cb0b54cd3a1
josegoms <josegomespinheiro26@gmail.com>
2025-03-07 10:48:53 -0300

    Add hover style to the square divs

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < maxDivs; i++) {
     let div = document.createElement("div");
-    div.className = "grid-item";
+    div.className = "grid-item hover-effect";
     container.appendChild(div);
-} 
+}
+
+const hoverEffects = document.querySelectorAll(".hover-effect");
+
+hoverEffects.forEach((effect) => {
+    effect.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "blue";
+    });
+
+    effect.addEventListener("mouseleave", (event) => {
+        event.target.style.backgroundColor = "white";
+    });
+});
